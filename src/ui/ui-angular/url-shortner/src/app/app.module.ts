9430a15e88170949b7d5c0c68cb054ccd7e25ce2
rahulgo8u <rrsimt.rahul@gmail.com>
2023-06-08 17:50:08 +0400

    UI for URL shortening in Angular

---
+++ src/ui/ui-angular/url-shortner/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
